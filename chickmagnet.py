40041d80f52e2a924d0e2795fb1a03a5f8135f4c
Sarah Eliha <saraheliha@gmail.com>
2021-03-24 17:34:11 +0100

    Use txt files for results and stuff

--- chickmagnet.py
+++ chickmagnet.py
@@ -22,12 +22,26 @@ while True:
         print('Failed. Trying again...')
 count = 0
 magnets = ''
+fin = open("magnets.txt", "a")
+try:
+    os.remove('recent_results.txt')
+except FileNotFoundError:
+    pass
+fin2 = open("recent_results.txt", "a")
 for result in results:
     #print(result.get('download'))
     count += 1
     magnets += str(result) + ' '
+    fin.write(result.get('download') + '\n')
+    fin2.write(result.get('download') + '\n')
 if count >= 100:
     print('\nIf the number of magnet links are greater than 100 then there might be more than 100 results. API limits results to maximum 100.')
+fin.close()
+fin2.close()
+#os.system('qbittorrent-nox ' + magnets)
 
-os.system('qbittorrent-nox ' + magnets)
+fin = open("history.txt", "a")
+fin.write(search_string + '\n')
+fin.close()
+
 print('\nAdded ' + str(count) + ' torrents.')
